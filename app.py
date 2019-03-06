dd80c41652b13043b8d3a0d6c75116d29aa3a5ed
ebinalex <ebinalexvalliyara@gmail.com>
2019-03-06 11:50:46 -0800

    index.html completed

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return"hello thala it's time for food"
+    return render_template("index.html")
 
 @app.route("/home")
 def home():
